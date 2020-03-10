c144411e2ebaec8872eaa11badac83b87611a0ad
clissiabovi <c195744@dac.unicamp.br>
2020-03-10 16:32:52 -0300

    11/12

--- src/main.c
+++ src/main.c
@@ -9,14 +9,38 @@
 
 int main() {
 
-  char c;
-
-  c = 1;
+  char c=0, atual=0, ant=0, igual=0;
+  int i=0, contadora=0;
 
   while (c != '\n') {
     scanf("%c", &c);
-  }
+	if(contadora==0){
+		atual=c;
+		ant=c;
+	}
+	else{
+		ant=atual;
+		atual=c;
+	}
+        if((atual=='.'|atual== ','| atual=='-'| atual==' '| atual=='\n') && (ant!='.' && ant!=',' && ant!=' ' && ant!='-')){
+		i++;
+		if(!(ant>='a' && ant<='z') && !(ant>='A' && ant<='Z')){
+			igual=1;
+}
+	}
+	else if((ant=='.'|ant== ','| ant=='!') && (atual!='.' && atual!=',' && atual!=' ' && atual!='-' && atual!='\n')){
 
-  printf("1\n");
+				if(igual==1 && !(atual>='a' && atual<='z')&& !(atual>='A' && atual<='Z')){
+					i--;
+				}				
+		igual=0;
+}
+    contadora++;
+  }
+  
+
+
+  printf("%d\n", i);
   return 0;
 }
+
